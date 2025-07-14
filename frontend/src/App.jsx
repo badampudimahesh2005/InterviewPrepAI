4d1111ef58c506afc1201b68e459738147a3a677
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-07-15 01:28:38 +0530

    defining routes, adding google fonts and some styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+
+import Login from './pages/Auth/Login';
+import Signup from './pages/Auth/Signup';
+import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Home/Dashboard';
+import InterviewPrep from './pages/InterviewPrep/InterviewPrep';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/interview-prep/:sessionId" element={<InterviewPrep />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        position='top-right'
+        reverseOrder={false}
+        toastOptions={{
+          className: '',
+          style: {
+            fontSize: '13px',
+          }
+        }}
+      />
+      {/* Add any global components or context providers here */}
+    </div>
   )
 }
 
-export default App
+export default App; 
