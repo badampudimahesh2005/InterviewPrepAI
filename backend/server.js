799e45bc85c9b80a90268e097bd6e84a225930cd
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-07-17 20:09:21 +0530

    backend project setup

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+// Middleware to handle CORS
+//what is cors?
+// CORS (Cross-Origin Resource Sharing) is a security feature that allows or 
+// restricts resources on a web page to be requested from another domain 
+// outside the domain from which the first resource was served.
+// It is used to allow or restrict requests from different origins (domains, protocols, or ports).
+
+app.use(
+    cors({
+    origin: "*", // Allow all origins, you can specify a specific origin if needed
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+})
+);
+
+//Middlewares
+app.use(express.json());
+
+//Routes
+
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
+
+
+//start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
