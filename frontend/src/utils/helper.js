de84b5b8845dd0774882838f5d772cddec06d1c8
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-08-15 19:40:45 +0530

    Creating Dashboard Summary Card UI

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -3,3 +3,17 @@ export const validateEmail = (email) => {
   return re.test(email);
 };
             
+
+
+export const getInitials = (title) => {
+  if (!title) return "";
+
+  const words = title.split(" ");
+  let initials = "";
+
+  for (let i = 0; i < Math.min(words.length, 2); i++) {
+    initials += words[i][0];
+  }
+
+  return initials.toUpperCase();
+};
