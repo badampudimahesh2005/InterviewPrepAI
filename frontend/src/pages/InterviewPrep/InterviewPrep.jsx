b87c7334a061802fe887bb336fc26c0691aa7e74
Mahesh@Webdev <maheshbadampudi31@gmail.com>
2025-08-15 20:41:33 +0530

    Creating Interview Q&A Card UI

--- frontend/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -1,11 +1,17 @@
 import  { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import moment from 'moment';
+import { LuCircleAlert, LuListCollapse } from 'react-icons/lu';
+import SpinnerLoader from '../../components/Loader/SpinnerLoader';
 import toast from 'react-hot-toast';
-import RoleInfoHeader from './components/RoleInfoHeader';
-import DashboardLayout from '../../components/layouts/DashboardLayout';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
+import DashboardLayout from '../../components/layouts/DashboardLayout';
+import RoleInfoHeader from './components/RoleInfoHeader';
+import QuestionCard from '../../components/Cards/QuestionCard';
+
+import { motion, AnimatePresence } from 'framer-motion';
+
 
 
 const InterviewPrep = () => {
@@ -77,7 +83,73 @@ const InterviewPrep = () => {
           : ""
       }
     />
+ <div className="container mx-auto pt-4 pb-4 px-4 md:px-0">
+      <h2 className="text-lg font-semibold color-black">Interview Q & A</h2>
 
+      <div className="grid grid-cols-12 gap-4 mt-5 mb-10">
+        <div
+          className={`col-span-12 ${
+            openLeanMoreDrawer ? "md:col-span-7" : "md:col-span-8"
+          }`}
+        >
+          <AnimatePresence>
+            {sessionData?.questions?.map((data, idx) => {
+              return (
+                <motion.div
+                  key={data._id || idx}
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, scale: 0.95 }}
+                  transition={{
+                    duration: 0.3,
+                    type: "spring",
+                    stiffness: 100,
+                    delay: idx * 0.1,
+                    damping: 15,
+                  }}
+                  layout
+                  layoutId={`question-${data._id || idx}`}
+                >
+                  <>
+                    <QuestionCard
+                      question={data?.question}
+                      answer={data?.answer}
+                      onLearnMore={() =>
+                        generateConceptExplanation(data.question)
+                      }
+                      isPinned={data?.isPinned}
+                      onTogglePin={() => toggleQuestionPinStatus(data._id)}
+                    />
+
+                    {!loading &&
+                      sessionData?.questions.length === idx + 1 && (
+                        <div className="flex items-center justify-center mt-5">
+                          <button
+                            className="flex items-center gap-3 text-sm text-white font-medium bg-black px-5 py-2 mr-2 rounded text-nowrap cursor-pointer"
+                            disabled={loading || isUpdateLoader}
+                            onClick={uploadMoreQuestions}
+                          >
+                            {isUpdateLoader ? (
+                              <SpinnerLoader />
+                            ) : (
+                              <LuListCollapse className="text-lg" />
+                            )}
+                            {" "}Load More
+                          </button>
+                        </div>
+                      )}
+                  </>
+                </motion.div>
+              );
+            })}
+          </AnimatePresence>
+        </div>
+      </div>
+
+               
+
+
+    </div>
    
   </DashboardLayout>
 );
